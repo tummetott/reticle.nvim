e048046543a9f2ec3301486adc510b42e55efc8d
Tummetott <19708025+Tummetott@users.noreply.github.com>
2024-01-19 10:36:40 +0100

    replaced deprecated set_option functions. Refactored some variables

--- lua/reticle/init.lua
+++ lua/reticle/init.lua
@@ -10,77 +10,78 @@ local enabled = { cursorline = false, cursorcolumn = false }
 local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
 local contains = vim.tbl_contains
-local set_option = vim.api.nvim_win_set_option
+local set_option = vim.api.nvim_set_option_value
+local get_option = vim.api.nvim_get_option_value
 local create_cmd = vim.api.nvim_create_user_command
 
 -- This function translates the semantic meaning of changing a window local
 -- option to the actual vim command.
-local change_option = function(window, opt, enable)
+local change_option = function(win, opt, enable)
     if opt == 'cursorline' and settings.always_highlight_number then
-        set_option(window, 'cursorlineopt', enable and 'both' or 'number')
-        set_option(window, 'cursorline', true)
+        set_option('cursorlineopt', enable and 'both' or 'number', { win = win } )
+        set_option('cursorline', true, { win = win })
     else
-        set_option(window, opt, enable)
+        set_option(opt, enable, { win = win })
     end
 end
 
 -- This function implements the logic whether and option is turned on or off on
 -- window enter, depending on the filetype of the window and the user
 -- configuration.
-local on_enter = function(opt, window)
-    local buffer = vim.api.nvim_win_get_buf(window)
-    local ft = vim.api.nvim_buf_get_option(buffer, 'filetype')
+local on_enter = function(opt, win)
+    local buf = vim.api.nvim_win_get_buf(win)
+    local ft = get_option('filetype', { buf = buf })
     if contains(settings.ignore[opt], ft) then
         -- TODO: make PR in trouble.nvim. cursorlineopt should be set to 'both'
         -- when cursorline is enabled in trouble windows.
         if ft == 'Trouble' and opt == 'cursorline' then
-            vim.api.nvim_win_set_option(window, 'cursorlineopt', 'both')
+            set_option('cursorlineopt', 'both', { win = win })
         end
         return
     elseif contains(settings.never[opt], ft) then
-        change_option(window, opt, false)
+        change_option(win, opt, false)
     elseif contains(settings.on_focus[opt], ft) or
         contains(settings.always[opt], ft) then
-        change_option(window, opt, true)
+        change_option(win, opt, true)
     elseif settings.follow[opt] then
-        change_option(window, opt, enabled[opt])
+        change_option(win, opt, enabled[opt])
     end
 end
 
 -- This function implements the logic whether and option is turned on or off on
 -- window leave, depending on the filetype of the window and the user
 -- configuration.
-local on_leave = function(opt, window)
-    local buffer = vim.api.nvim_win_get_buf(window)
-    local ft = vim.api.nvim_buf_get_option(buffer, 'filetype')
+local on_leave = function(opt, win)
+    local buf = vim.api.nvim_win_get_buf(win)
+    local ft = get_option('filetype', { buf = buf })
     if contains(settings.ignore[opt], ft) then
         return
     elseif contains(settings.always[opt], ft) then
-        change_option(window, opt, true)
+        change_option(win, opt, true)
     elseif contains(settings.never[opt], ft) or
         contains(settings.on_focus[opt], ft) or
         settings.follow[opt] then
-        change_option(window, opt, false)
+        change_option(win, opt, false)
     end
 end
 
 -- This wrapper function delays the on_enter() function in order to get the
 -- correct filetype
-local deferred_on_enter = function(opt, window)
+local deferred_on_enter = function(opt, win)
     vim.schedule(function()
         -- Exit if window does not exist anymore after deferring
-        if vim.fn.win_id2win(window) == 0 then return end
-        on_enter(opt, window)
+        if vim.fn.win_id2win(win) == 0 then return end
+        on_enter(opt, win)
     end)
 end
 
 -- This wrapper function delays the on_leave() function in order to get the
 -- correct filetype
-local deferred_on_leave = function(opt, window)
+local deferred_on_leave = function(opt, win)
     vim.schedule(function()
         -- Exit if window does not exist anymore after deferring
-        if vim.fn.win_id2win(window) == 0 then return end
-        on_leave(opt, window)
+        if vim.fn.win_id2win(win) == 0 then return end
+        on_leave(opt, win)
     end)
 end
 
@@ -94,17 +95,17 @@ local register_autocmds = function()
     local group = augroup('Reticle', { clear = true })
     autocmd(enter_events, {
         callback = function()
-            local window = vim.api.nvim_get_current_win()
-            deferred_on_enter('cursorline', window)
-            deferred_on_enter('cursorcolumn', window)
+            local win = vim.api.nvim_get_current_win()
+            deferred_on_enter('cursorline', win)
+            deferred_on_enter('cursorcolumn', win)
         end,
         group = group,
     })
     autocmd(leave_events, {
         callback = function()
-            local window = vim.api.nvim_get_current_win()
-            deferred_on_leave('cursorline', window)
-            deferred_on_leave('cursorcolumn', window)
+            local win = vim.api.nvim_get_current_win()
+            deferred_on_leave('cursorline', win)
+            deferred_on_leave('cursorcolumn', win)
         end,
         group = group,
     })
@@ -125,29 +126,29 @@ M.setup = function(user_config)
     enabled.cursorline = settings.on_startup.cursorline
     enabled.cursorcolumn = settings.on_startup.cursorcolumn
     -- Show cursorline and/or cursorcolumn on startup
-    local window = vim.api.nvim_get_current_win()
-    on_enter('cursorline', window)
-    on_enter('cursorcolumn', window)
+    local win = vim.api.nvim_get_current_win()
+    on_enter('cursorline', win)
+    on_enter('cursorcolumn', win)
 end
 
 -- UTILITY FUNCTIONS
 
 M.enable_cursorline = function()
-    local window = vim.api.nvim_get_current_win()
+    local win = vim.api.nvim_get_current_win()
     enabled.cursorline = true
-    on_enter('cursorline', window)
+    on_enter('cursorline', win)
 end
 
 M.disable_cursorline = function()
-    local window = vim.api.nvim_get_current_win()
+    local win = vim.api.nvim_get_current_win()
     enabled.cursorline = false
-    on_enter('cursorline', window)
+    on_enter('cursorline', win)
 end
 
 M.toggle_cursorline = function()
-    local window = vim.api.nvim_get_current_win()
+    local win = vim.api.nvim_get_current_win()
     enabled.cursorline = not enabled.cursorline
-    on_enter('cursorline', window)
+    on_enter('cursorline', win)
 end
 
 M.has_cursorline = function()
@@ -155,21 +156,21 @@ M.has_cursorline = function()
 end
 
 M.enable_cursorcolumn = function()
-    local window = vim.api.nvim_get_current_win()
+    local win = vim.api.nvim_get_current_win()
     enabled.cursorcolumn = true
-    on_enter('cursorcolumn', window)
+    on_enter('cursorcolumn', win)
 end
 
 M.disable_cursorcolumn = function()
-    local window = vim.api.nvim_get_current_win()
+    local win = vim.api.nvim_get_current_win()
     enabled.cursorcolumn = false
-    on_enter('cursorcolumn', window)
+    on_enter('cursorcolumn', win)
 end
 
 M.toggle_cursorcolumn = function()
-    local window = vim.api.nvim_get_current_win()
+    local win = vim.api.nvim_get_current_win()
     enabled.cursorcolumn = not enabled.cursorcolumn
-    on_enter('cursorcolumn', window)
+    on_enter('cursorcolumn', win)
 end
 
 M.has_cursorcolumn = function()
